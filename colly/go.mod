83faa8b48d26723155eb9ec54ef18906906d9911
jaronaearle <jaronaearle@gmail.com>
2023-06-19 23:41:02 -0600

    go stuff, probably will delete later

---
+++ colly/go.mod
@@ -0,0 +1,3 @@
+module bscolly
+
+go 1.20
--- colly/main.go
+++ colly/main.go
@@ -1,6 +1,37 @@
 package main
 
+import "fmt"
+
 func main() {
-    fmt.Println("Hello, Colly")
+	fmt.Println("Hello, Colly")
+
+	_, idx := BinSearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 2)
+	fmt.Println("idx is ", idx)
 }
 
+func BinSearch(nums []int, want int) (err error, idx int) {
+	if (len(nums) == 0) {
+		fmt.Println("empty")
+		err = fmt.Errorf("empty")
+		return
+	}
+
+	mid := len(nums) / 2
+	if (nums[mid] == want) {
+		idx = mid
+		fmt.Println("idx is mid")
+	}
+
+	if nums[mid] > want {
+		err, idx = BinSearch(nums[:mid], want)
+		fmt.Println("idx is great mid")
+	} else {
+		err, idx = BinSearch(nums[mid+1:], want)
+		fmt.Println("idx less mid")
+		idx += mid + 1
+	}
+
+	fmt.Println("idx is ", idx)
+
+	return
+}
